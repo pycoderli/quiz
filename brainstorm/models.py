b0e2db9991d2edc6510e7ba99cf5b4cd4386e0af
megha <megha9673@gmail.com>
2018-01-05 17:11:38 +0530

    model validator changed

--- brainstorm/models.py
+++ brainstorm/models.py
@@ -2,18 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.exceptions import ValidationError
 
-def validate_even(value):
-    i=0
-    while value!=0:
-        value=value/10
-        i=i+1
-    if value % 2 != 0:
-        raise ValidationError(
-            _('%(value)s is not an even number'),
-            params={'value': value},
-        )
+
+
 
 class level(models.Model):
     levelnumber = models.IntegerField(primary_key = True)
@@ -26,7 +17,7 @@ class level(models.Model):
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     college = models.CharField(max_length=100)
-    phoneno = models.IntegerField(validators=[validate_even])
+    phoneno = models.IntegerField()
     current_level =  models.ForeignKey('level', on_delete=models.CASCADE, default=1) 
     levelentry = models.DateTimeField(auto_now=True)
     def __str__(self):
